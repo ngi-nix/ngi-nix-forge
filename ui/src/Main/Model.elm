7f01c8fbf3cd29e1df258fd7e070a342da86fe87
Julien Moutinho <julm@sourcephile.fr>
2026-03-10 20:12:04 +0100

    +user/control(ui): init routing to selected app

--- ui/src/Main/Model.elm
+++ ui/src/Main/Model.elm
@@ -6,5 +6,12 @@ import Main.Route exposing (..)
 import Main.Select.Model exposing (ModelSelect)
 
 
+
+-- | Explanation(extensibility):
+-- `Model` is a sum-type to support different models
+-- though this is no longer used, it may be in a close future,
+-- hence keep it a little bit longer until requirements have settled.
+
+
 type Model
     = Model_Select ModelSelect
