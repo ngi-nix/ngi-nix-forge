a8d641c82fe0035bf18e70b04c792a88d4712164
Sizhe Zhao <prc.zhao@outlook.com>
2026-03-27 19:03:25 +0800

    recipes/packages/tslib: init at 1.24

---
+++ recipes/packages/tslib/recipe.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+rec {
+  name = "tslib";
+  version = "1.24";
+  description = "Touchscreen access library";
+  homePage = "http://www.tslib.org/";
+  mainProgram = "";
+  license = lib.licenses.lgpl21;
+
+  source = {
+    git = "github:libts/tslib/${version}";
+    hash = "sha256-WrzOTZlceYnFXi5AI5vb+ZDSRoqUDk/yyCdBUWKn0sM=";
+  };
+
+  build.standardBuilder = {
+    enable = true;
+    requirements.native = [
+      pkgs.cmake
+    ];
+  };
+}
